29efea5f7723e8ba24a393bf859475caac07c0a5
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-09 15:17:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,12 +12,3 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def move(board, index, value)
-  board[index] = value
-end
-
-
-def  move(board, index, player = "X")
-  board[index] = player
-end
