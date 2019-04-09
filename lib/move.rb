3d360120d42a4c4e954e63bbd357409d0b75c240
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-09 14:51:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,10 +13,10 @@ def input_to_index(user_input)
 end
 
 board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
+def move(board, index, value)
+  board[index] = value
 end
 
-update_array_at_with(board, 0, "X")
+move(board, 0, "X")
 # The element at index 0 of array 'board' is set to the value "X"
 board #=> ["X", " ", " "]
